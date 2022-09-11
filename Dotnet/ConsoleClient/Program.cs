7198495809723d618c5edc92f39a20f0e2c216fd
jke94 <jcarracedo.dev@gmail.com>
2022-09-11 10:59:32 +0200

    Add logic to call native method in manage code.

--- Dotnet/ConsoleClient/Program.cs
+++ Dotnet/ConsoleClient/Program.cs
@@ -1,9 +1,29 @@
 ﻿namespace ConsoleClient
 {
+    using NativeWrapper;
+
     public class Program
     {
         public static void Main()
         {
+            Settings mySettings;
+
+            mySettings.input_path = "TheInputPath";
+            mySettings.first_file = "FirstFile";
+            mySettings.second_file = "SecondFile";
+            mySettings.mask = "TheMask";
+            mySettings.log_path = "LogPath";
+            mySettings.count_accepted = 1994;
+            mySettings.confidence = 1994.1994;
+            mySettings.device = "TheDevice";
+
+            Result myResult;
+
+            var result = LibWrapper.ProcessInput(
+                ref mySettings, out myResult);
+
+            Console.WriteLine($"ProcessInput: {result}");
+
             Console.WriteLine("Hello world!");
         }
     }
--- Dotnet/NativeWrapper/LibWrapper.cs
+++ Dotnet/NativeWrapper/LibWrapper.cs
@@ -7,18 +7,25 @@
     {
         [MarshalAs(UnmanagedType.LPStr)]
         public string input_path;
+
         [MarshalAs(UnmanagedType.LPStr)]
         public string first_file;
+
         [MarshalAs(UnmanagedType.LPStr)]
         public string second_file;
+
         [MarshalAs(UnmanagedType.LPStr)]
         public string mask;
+
         [MarshalAs(UnmanagedType.LPStr)]
         public string log_path;
+
         [MarshalAs(UnmanagedType.I4)]
         public int count_accepted;
+
         [MarshalAs(UnmanagedType.R8)]
         public double confidence;
+        
         [MarshalAs(UnmanagedType.LPStr)]
         public string device;
     }
@@ -28,10 +35,13 @@
     {
         [MarshalAs(UnmanagedType.I4)]
         public int count;
-        [MarshalAs(UnmanagedType.SysInt)]
+
+        // [MarshalAs(UnmanagedType.SysInt)]
         // public IntPtr foos;
-        // [MarshalAs(UnmanagedType.I1)]
+
+        [MarshalAs(UnmanagedType.I1)]
         public bool is_preprocessed;
+        
         [MarshalAs(UnmanagedType.R8)]
         public double duration;
     }
